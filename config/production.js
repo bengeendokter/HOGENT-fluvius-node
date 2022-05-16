439d9a4cd55caf25b6659467db1da85ef3de471d
CasDS98 <79106753+CasDS98@users.noreply.github.com>
2022-05-16 17:26:01 +0200

    fix

--- config/production.js
+++ config/production.js
@@ -8,4 +8,28 @@ module.exports = {
 		origins: ['http://localhost:3000'],
 		maxAge: 3 * 60 * 60,
 	},
+	database: {
+    client: 'mysql2',
+    host: 'localhost',
+    port: 3306,
+    name: 'fluvius',
+  },
+	pagination: {
+    limit: 100,
+    offset: 0,
+  },
+	auth: {
+		argon: {
+			saltLength: 16,
+			hashLength: 32,
+			timeCost: 3,
+			memoryCost: 64 * 1024
+		},
+    jwt: {
+      secret: 'eenveeltemoeilijksecretdatniemandooitzalradenandersisdesitegehacked',
+      expirationInterval: 60 * 60 * 1000, // ms (1 hour)
+      issuer: 'mvo.fluvius.be',
+      audience: 'mvo.fluvius.be',
+    },
+	}
 };
